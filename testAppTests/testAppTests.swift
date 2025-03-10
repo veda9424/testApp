fd54f69510ea287f82a975afd0a96b1f003e0ca7
veda9424 <vedantdivekar123@gmail.com>
2025-03-10 22:02:32 +0530

    initial code commit

---
+++ testAppTests/testAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  testAppTests.swift
+//  testAppTests
+//
+//  Created by Vedant Ravindra Divekar  on 10/03/25.
+//
+
+import Testing
+@testable import testApp
+
+struct testAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
